68d2e9f8641cc204db29c5e275311b9a019a2318
Nelson <nsaularaujof@gmail.com>
2025-12-08 17:08:27 -0700

    Add love and unlove button selectors

--- src/connectors/ibroadcast.ts
+++ src/connectors/ibroadcast.ts
@@ -14,5 +14,11 @@ Connector.durationSelector = '.mgr-player-duration';
 
 Connector.trackArtSelector = '.mgr-player-artwork-image';
 
+Connector.loveButtonSelector = () =>
+	Util.hasElementClass(['.mgr-player-play'], 'icon-thumbs-up');
+
+Connector.unloveButtonSelector = () =>
+	Util.hasElementClass(['.mgr-player-play'], 'icon-thumbs-up-on');
+
 Connector.isPlaying = () =>
 	Util.hasElementClass(['.mgr-player-play'], 'icon-player-pause');
