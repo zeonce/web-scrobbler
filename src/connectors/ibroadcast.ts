ca8c98a82946294adc92d13721be5a99fc3a7cb0
Nelson <nsaularaujof@gmail.com>
2025-12-08 17:40:50 -0700

    Refactor loveButtonSelector and unloveButtonSelector

--- src/connectors/ibroadcast.ts
+++ src/connectors/ibroadcast.ts
@@ -11,14 +11,12 @@ Connector.albumSelector = '.mgr-player-album';
 Connector.currentTimeSelector = '.mgr-player-current-time';
 
 Connector.durationSelector = '.mgr-player-duration';
- 
+
 Connector.trackArtSelector = '.mgr-player-artwork-image';
 
-Connector.loveButtonSelector = () =>
-	Util.hasElementClass(['.mgr-player-play'], 'icon-thumbs-up');
+Connector.loveButtonSelector = '.mgr-player-play icon-thumbs-up';
 
-Connector.unloveButtonSelector = () =>
-	Util.hasElementClass(['.mgr-player-play'], 'icon-thumbs-up-on');
+Connector.unloveButtonSelector = '.mgr-player-play icon-thumbs-up-on';
 
 Connector.isPlaying = () =>
 	Util.hasElementClass(['.mgr-player-play'], 'icon-player-pause');
